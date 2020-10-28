e5ca51a0e292a4dc542b74d1113d979debbdd9ef
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-10-27 18:34:55 -0700

    Adjust camera fall values to prevent player from ever going off screen

--- Assets/__Scripts/CameraFollow.cs
+++ Assets/__Scripts/CameraFollow.cs
@@ -8,11 +8,16 @@ public class CameraFollow : MonoBehaviour
     [Header("Set in Inspector")]
     [SerializeField] bool followDown = true;
     [SerializeField] float smoothTime = 0.4f;
+    [SerializeField] float cameraOffsetY = 2.0f;
+
+    [SerializeField]
+    [Range(0, 1)] float minViewportDistFromEdge = 0.1f;     // always maintain player character at this viewport distance from the edge (10% inward)
 
     private Vector2 velocity;
     private Vector3 initPos;
     private GameObject player;
     private float minY;
+    private Camera cam;
 
     private void Awake()
     {
@@ -24,6 +29,7 @@ public class CameraFollow : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -34,17 +40,34 @@ public class CameraFollow : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SmoothFollow();
+    }
+
+    private void SmoothFollow()
+    {
         // if not following camera down, and the player is lower than the min position, don't follow
         if (!followDown && player.transform.position.y > minY)
         {
             return;
+        } else if (OozeEscape.Instance.CurrState == OozeEscape.GameState.Play)
+        {
+            float playerViewportY = cam.WorldToViewportPoint(player.transform.position).y;
+            float smoothTimeScale = Mathf.Min(Mathf.Min(Mathf.Abs(1 - playerViewportY), Mathf.Abs(0 - playerViewportY)) / 0.1f, 1f);
+
+            float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + cameraOffsetY, ref velocity.y, smoothTime*smoothTimeScale);
+            // never go below initial y position
+            posY = Mathf.Max(posY, initPos.y);
+
+            transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+            minY = Mathf.Max(minY, posY - 6f);
         }
-
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTime);
-        // never go below initial y position
-        posY = Mathf.Max(posY, initPos.y);
-
-        transform.position = new Vector3(transform.position.x, posY, transform.position.z);
-        minY = Mathf.Max(minY, posY - 6f);
     }
+
+    /*
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+            Gizmos.DrawWireCube(transform.position, new Vector3(1f, 1f, 1f));
+    }
+    */
 }
